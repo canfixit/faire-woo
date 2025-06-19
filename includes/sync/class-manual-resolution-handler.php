236a7f0afbcae73b95e767f9a04ba5000f78b327
haibin <haibin_li@users.noreply.github.com>
2025-06-20 09:48:09 +1000

    Refactor ManualResolutionHandler to retrieve all pending resolutions across orders. Update method to return an associative array of order IDs and their corresponding resolution queues, improving the handling of manual resolutions.

--- includes/sync/class-manual-resolution-handler.php
+++ includes/sync/class-manual-resolution-handler.php
@@ -26,13 +26,35 @@ class ManualResolutionHandler {
     }
 
     /**
-     * Get pending resolutions for an order.
+     * Get all pending resolutions across all orders.
      *
-     * @param int $order_id The WooCommerce order ID.
-     * @return array Array of pending resolutions.
+     * @return array An associative array where keys are order IDs and values are the resolution queues.
      */
-    public function get_pending_resolutions($order_id) {
-        return array();
+    public function get_pending_resolutions() {
+        $args = [
+            'post_type'   => 'shop_order',
+            'post_status' => 'any',
+            'meta_key'    => '_faire_manual_resolution_queue',
+            'meta_compare' => 'EXISTS',
+            'posts_per_page' => -1,
+            'fields' => 'ids',
+        ];
+
+        $order_ids = get_posts($args);
+        $all_pending = [];
+
+        if (empty($order_ids)) {
+            return $all_pending;
+        }
+
+        foreach ($order_ids as $order_id) {
+            $queue = get_post_meta($order_id, '_faire_manual_resolution_queue', true);
+            if (!empty($queue)) {
+                $all_pending[$order_id] = $queue;
+            }
+        }
+        
+        return $all_pending;
     }
 
     /**
